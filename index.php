fc13ab53a2a814c9d3169ac983408e3c8bd0bdd4
omkarhcode <omkarh.code@gmail.com>
2024-01-10 17:16:05 +0530

    Add global and header stylesheets

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <title>Responsive Nav bar</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/global.css">
+    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/header.css">
 
 </head>
 
