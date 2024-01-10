5211c40d8eec00b724cd54bca8b67941c6dd4187
omkarhcode <omkarh.code@gmail.com>
2024-01-10 17:38:31 +0530

    Add dropdown arrow SVG and update section styling

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/global.css">
     <link rel="stylesheet" href="style/header.css">
-    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/section.css">
 
 </head>
 
@@ -40,18 +40,17 @@
             <h2>Contact Us</h2>
             <p>When we aren’t changing landscapes, we’re making headlines.</p>
             <h3>Send us an Enquiry</h3>
-
             <form id="contact-form">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" placeholder="Your Name" required>
+                <!-- <label for="name">Name:</label> -->
+                <input type="text" id="name" name="name" placeholder="Name" required>
 
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Your Email" required>
 
-                <label for="mobile">Mobile:</label>
-                <input type="tel" id="mobile" name="mobile" placeholder="Your Mobile" required>
+                <input type="email" id="email" name="email" placeholder="Email Address" required>
 
-                <label for="project">Project:</label>
+
+                <input type="tel" id="mobile" name="mobile" placeholder="Mobile Number" required>
+
+
                 <select id="project" name="project" required>
                     <option value="" selected disabled>Select Project</option>
                     <option value="project1">Project 1</option>
