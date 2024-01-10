90ec563408030f548816f85d9560db1240a8dee7
omkarhcode <omkarh.code@gmail.com>
2024-01-10 14:43:39 +0530

    git init

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Responsive Nav bar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body id="body">
+
+    <header class="header">
+        <a href="#" class="logo">LOGO</a>
+
+        <input type="checkbox" id="check">
+        <label for="check" class="menu-icons">
+            <img src="assets/menu.svg" alt="" id="hamburger">
+            <img src="assets/close.png" alt="" id="close">
+        </label>
+
+        <nav class="navbar">
+            <a href="#">PROJECTS</a>
+            <a class="" href="#">ABOUT</a>
+            <a class="" href="#">NRI AREA</a>
+            <a class="" href="#">NEWS & THOUGHTS</a>
+            <a class="" href="#">CONTACT</a>
+        </nav>
+    </header>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
